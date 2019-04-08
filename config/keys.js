9b48b85090f76e629e32c5b1ce33ec25b947f7e3
Andrea <andreabonola@gmail.com>
2019-04-08 18:24:47 -0400

    added commits

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+mongoURI: "mongodb+srv://andrea:andrea123@cluster0-p89s0.mongodb.net/test"
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose= require('mongoose');
+const bodyParser= require('body-parser');
+
+const app = express();
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+console.log(db);
+
+//Connect to Mongo
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
