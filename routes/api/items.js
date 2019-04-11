9f537e5f62080c51e4c9a55a664bf7db4c34e9d6
Andrea <andreabonola@gmail.com>
2019-04-10 20:17:37 -0400

    Added POST and DELETE to items.js

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,4 +15,26 @@ router.get('/', (req, res) => {
     .then(items => res.json(items)) 
 });
 
+//@route POST api/items
+//@desc Create new Items
+//@access Public
+
+router.post('/', (req, res) => {
+    const newItem = new Items({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+});
+
+//@route DELETE api/items/:id
+//@desc Get ALL Items
+//@access Public
+
+router.delete('/:id', (req, res) => {
+    Items.findById(req.params.id)
+			.then(item => item.remove().then(() => res.json({success: true})))
+			.catch(err => res.status(404).json({success: false}));
+});
+
+
 module.exports = router;
