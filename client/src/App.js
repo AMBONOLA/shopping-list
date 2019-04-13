c72a3de1883875f88a9f7ae90e204aa0bba752b1
Andrea <andreabonola@gmail.com>
2019-04-13 13:53:46 -0400

    Added a ShoppingList component. Wrote css for the CSSTransiton.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -7,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppNavbar/>
+        <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
