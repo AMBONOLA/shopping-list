348d9b41abc3e287489191049ec4474fa5016b82
Andrea <andreabonola@gmail.com>
2019-10-16 22:07:49 -0400

    fixed and updated stuff

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ console.log(db);
 
 //Connect to Mongo
 mongoose
-.connect(db, { useNewUrlParser: true })
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
 //Use Routes
 app.use('/api/items', items);
 
-const port = 5000;
+const port = 3001;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
